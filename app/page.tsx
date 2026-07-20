0f58c9ea40169dda0cd5af5bb6af44dc1e26e20c
ratanak <ratanakcoding.it@gmail.com>
2026-07-20 23:20:46 +0700

    pull from ratanak chan

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import DashboardPage from "@/components/teacher/DashboardPage";
 
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <DashboardPage/>
+     <h1>Home page</h1>
     </div>
   );
 }
